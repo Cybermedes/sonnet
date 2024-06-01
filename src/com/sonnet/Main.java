64d42ee741d52649a8387ab9db3c83c018f6892e
Lucas Paiva <lpaivamatiazzi@gmail.com>
2024-06-01 18:30:55 -0300

    Send a http request and get file as byte stream

--- src/com/sonnet/Main.java
+++ src/com/sonnet/Main.java
@@ -1,8 +1,26 @@
 package com.sonnet;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World from Java " + Runtime.version());
+        // Read Shakespeare's Sonnets text file online
+        URI sonnetsURI = URI.create("https://www.gutenberg.org/cache/epub/1041/pg1041.txt");
+        HttpRequest request = HttpRequest.newBuilder(sonnetsURI).GET().build();
+        try (HttpClient client = HttpClient.newBuilder().build()) {
+            HttpResponse<InputStream> response = client.send(request,
+                    HttpResponse.BodyHandlers.ofInputStream());
+            InputStream inputStream = response.body();
+            inputStream.close();
+        } catch (IOException | InterruptedException e) {
+            System.err.format("%s%n", e);
+        }
+
     }
 }
