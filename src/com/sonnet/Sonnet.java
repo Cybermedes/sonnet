698192ed541c8e2ef2d686f9dc84f81aa4813255
Lucas Paiva <lpaivamatiazzi@gmail.com>
2024-06-01 18:56:42 -0300

    Create a class to represent a sonnet and store its lines

---
+++ src/com/sonnet/Sonnet.java
@@ -0,0 +1,14 @@
+package com.sonnet;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Sonnet {
+
+    // Store the sonnet lines in a list
+    private List<String> lines = new ArrayList<>();
+
+    public void add(String line) {
+        lines.add(line);
+    }
+}
