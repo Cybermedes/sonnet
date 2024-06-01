2f9326e979232fe730ecc19bef83ff98b1bf8443
Lucas Paiva <lpaivamatiazzi@gmail.com>
2024-06-01 18:49:25 -0300

    Create a class to store sonnets in an ArrayList

---
+++ src/com/sonnet/Sonnet.java
@@ -0,0 +1,14 @@
+package com.sonnet;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Sonnet {
+
+    // Store all the Sonnets in a List
+    private List<String> lines = new ArrayList<>();
+
+    public void add(String line) {
+        lines.add(line);
+    }
+}
