822e47f66c934ccd28c5be30f7dd61dd57321dee
Lucas Paiva <lpaivamatiazzi@gmail.com>
2024-06-01 20:16:41 -0300

    feat: add method to read sonnets
    Method read only the sonnet body content and
    discard empty/ blank lines and the sonnet number

--- src/com/sonnet/Main.java
+++ src/com/sonnet/Main.java
@@ -6,6 +6,8 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
@@ -13,14 +15,36 @@ public class Main {
         // Read Shakespeare's Sonnets text file online
         URI sonnetsURI = URI.create("https://www.gutenberg.org/cache/epub/1041/pg1041.txt");
         HttpRequest request = HttpRequest.newBuilder(sonnetsURI).GET().build();
+
         try (HttpClient client = HttpClient.newBuilder().build()) {
+
             HttpResponse<InputStream> response = client.send(request,
                     HttpResponse.BodyHandlers.ofInputStream());
+
             InputStream inputStream = response.body();
+            readFileContent(inputStream);
             inputStream.close();
         } catch (IOException | InterruptedException e) {
             System.err.format("%s%n", e);
         }
 
+    }
+
+    private static void readFileContent(InputStream inputStream) throws IOException {
+        // The text start at line 30, before this line there is only technical info
+        int start = 30;
+
+        List<Sonnet> sonnets = new ArrayList<>();
+
+        var reader = new SonnetReader(inputStream);
+        reader.skipLines(start);
+        var sonnet = reader.readNextSonnet();
+        while (sonnet != null) {
+            sonnets.add(sonnet);
+            sonnet = reader.readNextSonnet();
+        }
+        reader.close();
+
+        System.out.println("Number of sonnets = " + sonnets.size());
     }
 }
--- src/com/sonnet/SonnetReader.java
+++ src/com/sonnet/SonnetReader.java
@@ -28,7 +28,7 @@ public class SonnetReader extends BufferedReader {
         }
 
         // If the line has this text, end of the file
-        if (line.equals("*** END OF THE PROJECT GUTENBERG EBOOK THE SONNETS ***")) {
+        if (line.equals("*** END OF THE PROJECT GUTENBERG EBOOK SHAKESPEARE'S SONNETS ***")) {
             return null;
         }
 
@@ -36,7 +36,22 @@ public class SonnetReader extends BufferedReader {
         do {
             line = readLine();
         } while (line.isBlank());
-
         return line;
+    }
+
+    // Read all the sonnet lines, if a blank line is met, then it has been fully read
+    public Sonnet readNextSonnet() throws IOException {
+        String line = skipSonnetHeader();
+        if (line == null) {
+            return null;
+        } else {
+            var sonnet = new Sonnet();
+            while (!line.isBlank()) {
+                sonnet.add(line);
+                line = readLine();
+            }
+            return sonnet;
+        }
+
     }
 }
